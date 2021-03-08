3bdb916eb2dfabb978c9510366cb39b31402e0df
David Luposchainsky <dluposchainsky@gmail.com>
2021-03-08 12:50:16 +0100

    Move BME280 into its own file

---
+++ esp8266/bme280_temperature_humidity_pressure.h
@@ -0,0 +1,25 @@
+#ifndef BME280_TEMPERATURE_HUMIDITY_PRESSURE_H
+#define BME280_TEMPERATURE_HUMIDITY_PRESSURE_H
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 thpSensor;
+
+void setup_bme280() {
+    unsigned int status = thpSensor.begin();
+    // You can also pass in a Wire library object like &Wire2
+    // status = bme.begin(0x76, &Wire2)
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(thpSensor.sensorID(),16);
+        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+        Serial.print("        ID of 0x60 represents a BME 280.\n");
+        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        while (1) delay(100);
+    }
+}
+
+#endif
--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,12 +1,9 @@
 #include "bh1750_light.h"
 #include "si7021_temperature_humidity.h"
-
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#define SEALEVELPRESSURE_HPA (1013.25)
+// #include "bme280_temperature_humidity_pressure.h"
 
 
-Adafruit_BME280 thpSensor;
+
 void setup() {
     Serial.begin(115200);
     while (!Serial) {
@@ -15,18 +12,7 @@ void setup() {
     setup_light_sensor();
     pinMode(LED_BUILTIN, OUTPUT);
 
-    unsigned status = thpSensor.begin();
-    // You can also pass in a Wire library object like &Wire2
-    // status = bme.begin(0x76, &Wire2)
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(thpSensor.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(100);
-    }
+
 }
 
 void builtin_led_on(bool state) {
