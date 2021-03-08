9c2adac70509870bfd0428fb7060e0b58b277c18
David Luposchainsky <dluposchainsky@gmail.com>
2021-03-08 13:06:57 +0100

    Add hue-rotating LED

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,6 +1,7 @@
 #include "bh1750_light.h"
 #include "si7021_temperature_humidity.h"
 // #include "bme280_temperature_humidity_pressure.h"
+#include "ws2811_rgb_led.h"
 
 
 
@@ -9,23 +10,27 @@ void setup() {
     while (!Serial) {
         delay(10);
     }
+
     setup_light_sensor();
+    setup_rgb_led();
     pinMode(LED_BUILTIN, OUTPUT);
-
-
 }
 
 void builtin_led_on(bool state) {
     digitalWrite(LED_BUILTIN, state ? LOW : HIGH);
 }
 
+int hue = 0;
 void loop() {
     builtin_led_on(true);
     float lux = measure_light_level_lux();
     float celsius = measure_temperature_celsius();
     float humidity = measure_rel_humidity();
     Serial.printf("%f,%f,%f\n", lux, celsius, humidity);
-    delay(100);
+    rgb_led.setHue(hue);
+    hue = (hue + 20) % 256;
+    FastLED.show();
+    delay(50);
     builtin_led_on(false);
     delay(1000);
 }
---
+++ esp8266/ws2811_rgb_led.h
@@ -0,0 +1,15 @@
+#ifndef WS2811_RGB_LED_H
+#define WS2811_RGB_LED_H
+
+#include <FastLED.h>
+#define NUM_LEDS 1
+#define DATA_PIN 4
+
+CRGB leds[NUM_LEDS];
+CRGB& rgb_led = leds[0];
+void setup_rgb_led() {
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.setBrightness(16);
+}
+
+#endif
