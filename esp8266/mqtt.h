fd75a60e43b97547515f24080e3b9c337ccf5bf2
David Luposchainsky <dluposchainsky@gmail.com>
2021-03-12 14:05:24 +0100

    Move sensitive data into its own folder

--- esp8266/mqtt.h
+++ esp8266/mqtt.h
@@ -1,7 +1,7 @@
-#include "hidden.h"
 #include "lib/bh1750_light.h"
 #include "lib/bme280_temperature_humidity_pressure.h"
 #include "lib/ws2811_rgb_led.h"
+#include "sensitive/settings.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
